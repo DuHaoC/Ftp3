d6e075e7dd6698bcc8b7e8ea9e4b04046d9bfa1d
HC <HC@DHC>
2017-04-01 10:07:07 +0800

    添加信息

--- src/com/newer/ftp3/App.java
+++ src/com/newer/ftp3/App.java
@@ -3,7 +3,8 @@ package com.newer.ftp3;
 public class App {
 
 	public static void main(String[] args) {
-		
+
+		AppFrame frame = new AppFrame();
 
 	}
 
--- src/com/newer/ftp3/AppFrame.java
+++ src/com/newer/ftp3/AppFrame.java
@@ -2,8 +2,17 @@ package com.newer.ftp3;
 
 import javax.swing.JFrame;
 
-public class AppFrame extends JFrame{
- public AppFrame(){
-	 setSize(500,500);
- }
+public class AppFrame extends JFrame {
+	/*
+	 * 构造方法
+	 */
+	public AppFrame() {
+		setSize(500, 500);
+		setTitle("五子棋");
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+
+	}
 }
